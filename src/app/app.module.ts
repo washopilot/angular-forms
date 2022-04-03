acfa9e52c925104c29b48aa00e04e2b49315a6c9
Fernando Chicaiza <washopilot@yahoo.com>
2022-04-02 22:03:52 -0500

    demo-form-sku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DemoFormSkuComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-sku',
+  templateUrl: './demo-form-sku.component.html',
+  styleUrls: ['./demo-form-sku.component.css'],
+})
+export class DemoFormSkuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: any): void {
+    console.log('you submitted value: ', form);
+  }
+}
