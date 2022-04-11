caf813327955f396b0e432649a4bf028a64a8584
Fernando Chicaiza <washopilot@yahoo.com>
2022-04-10 21:51:30 -0500

    formControl and formGroup (error)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
 
 @NgModule({
-  declarations: [AppComponent, DemoFormSkuComponent],
+  declarations: [AppComponent, DemoFormSkuComponent, DemoFormSkuWithBuilderComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
